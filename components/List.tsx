31ae7aca698a7b8bdcc4019518a3ae999b315a1a
Marko Seppänen <marko@marko.xyz>
2023-09-13 10:42:31 +0300

    2-http-b

--- components/List.tsx
+++ components/List.tsx
@@ -2,17 +2,53 @@ import { FlatList } from 'react-native'
 import { ListItem } from './ListItem'
 import { useEffect, useState } from 'react'
 
-const url =
-  'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json'
+const url = 'http://media.mw.metropolia.fi/wbma'
+
+interface MediaItem {
+  file_id: number
+  filename: string
+  filesize: number
+  title: string
+  description: string
+  user_id: number
+  media_type: string
+  mime_type: string
+  time_added: string
+}
+
+export interface MediaFile {
+  file_id: number
+  filename: string
+  filesize: number
+  title: string
+  description: string
+  user_id: number
+  media_type: string
+  mime_type: string
+  time_added: string
+  thumbnails: {
+    w640: string
+    w320: string
+    w160: string
+  }
+}
 
 export const List = () => {
-  const [mediaArray, setmediaArray] = useState([])
+  const [mediaArray, setmediaArray] = useState<MediaFile[]>([])
 
   const loadMedia = async () => {
     try {
-      const response = await fetch(url)
+      const response = await fetch(`${url}/media`)
       const responseJson = await response.json()
-      setmediaArray(responseJson)
+
+      const media = await Promise.all(
+        responseJson.map(async (item: MediaItem) => {
+          const response = await fetch(`${url}/media/${item.file_id}`)
+          const mediaJson = await response.json()
+          return mediaJson
+        })
+      )
+      setmediaArray(media)
     } catch (error) {
       console.error('Error loading media', error)
     }
@@ -26,7 +62,7 @@ export const List = () => {
     <FlatList
       data={mediaArray}
       keyExtractor={(_item, index) => index.toString()}
-      renderItem={({ item }) => <ListItem singleItem={item} />}
+      renderItem={({ item }) => <ListItem {...item} />}
     />
   )
 }
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,25 +1,20 @@
 import { Image, Text, TouchableOpacity, View } from 'react-native'
 import { listItemStyle } from '../styles'
+import { MediaFile } from './List'
 
-type ListItemProps = {
-  singleItem: {
-    description: string
-    thumbnails: {
-      w160: string
-    }
-    title: string
-  }
+const uploads = 'http://media.mw.metropolia.fi/wbma/uploads'
+
+export const ListItem = ({ description, thumbnails, title }: MediaFile) => {
+  return (
+    <TouchableOpacity style={listItemStyle.card}>
+      <Image
+        style={{ width: 100, height: 100 }}
+        source={{ uri: `${uploads}/${thumbnails.w320}` }}
+      />
+      <View style={listItemStyle.textbox}>
+        <Text style={listItemStyle.textboxHeader}>{title}</Text>
+        <Text style={listItemStyle.text}>{description}</Text>
+      </View>
+    </TouchableOpacity>
+  )
 }
-
-export const ListItem = ({ singleItem }: ListItemProps) => (
-  <TouchableOpacity style={listItemStyle.card}>
-    <Image
-      style={{ width: 100, height: 100 }}
-      source={{ uri: singleItem.thumbnails.w160 }}
-    />
-    <View style={listItemStyle.textbox}>
-      <Text style={listItemStyle.textboxHeader}>{singleItem.title}</Text>
-      <Text style={listItemStyle.text}>{singleItem.description}</Text>
-    </View>
-  </TouchableOpacity>
-)
