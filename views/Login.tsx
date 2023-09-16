d685895049ec789f7165592b4134c61dcd17b36b
Marko Seppänen <marko@marko.xyz>
2023-09-16 20:29:13 +0300

    5-dummylogin done.

--- views/Login.tsx
+++ views/Login.tsx
@@ -1,14 +1,29 @@
+import { useContext, useEffect } from 'react'
 import { View, Text, Button } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 import { containerStyle } from '../styles/mainStyles'
-import { useContext } from 'react'
 import { MainContext } from '../contexts/MainContext'
 
 export const Login = () => {
   const { setIsLoggedIn } = useContext(MainContext)
 
-  const logIn = () => {
+  const logIn = async () => {
     setIsLoggedIn(true)
+    await AsyncStorage.setItem('userToken', 'abc')
   }
+
+  const checkToken = async () => {
+    const userToken = await AsyncStorage.getItem('userToken')
+    if (userToken === 'abc') {
+      setIsLoggedIn(true)
+    }
+  }
+
+  useEffect(() => {
+    checkToken()
+  }, [checkToken])
+
   return (
     <View style={containerStyle.container}>
       <Text>Login</Text>
--- views/Profile.tsx
+++ views/Profile.tsx
@@ -1,13 +1,16 @@
-import { Button, SafeAreaView, Text } from 'react-native'
-import { containerStyle } from '../styles/mainStyles'
 import { useContext } from 'react'
+import { Button, SafeAreaView, Text } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import { containerStyle } from '../styles/mainStyles'
 import { MainContext } from '../contexts/MainContext'
 
 export const Profile = () => {
   const { setIsLoggedIn } = useContext(MainContext)
 
-  const logout = () => {
+  const logout = async () => {
     setIsLoggedIn(false)
+    await AsyncStorage.clear()
   }
   return (
     <SafeAreaView style={containerStyle.container}>
